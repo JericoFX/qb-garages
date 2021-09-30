5fa79c863bfd37a2be9041301fc4a161d6732b31
JericoFX <56100605+JericoFX@users.noreply.github.com>
2021-09-30 13:47:23 -0300

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,11 +1,11 @@
 local CacheInfo = {}
-QBCore.Functions.CreateCallback("fx-garage:server:GetVehicles",function(source, cb, garage)
+QBCore.Functions.CreateCallback("qb-garages:server:GetVehicles",function(source, cb, garage)
     local Player = QBCore.Functions.GetPlayer(source)
     local GetVehicles = exports.oxmysql:fetchSync("SELECT * FROM player_vehicles WHERE citizenid = ? AND garage = ?",{Player.PlayerData.citizenid, garage})
     cb(GetVehicles)
 end)
 
-QBCore.Functions.CreateCallback("fx-garage:server:GetVehicleProps",function(source, cb, plate)
+QBCore.Functions.CreateCallback("qb-garages:server:GetVehicleProps",function(source, cb, plate)
     local src = source
     local properties = {}
     local result = exports.oxmysql:fetchSync('SELECT mods FROM player_vehicles WHERE plate= ?', {plate})
@@ -13,7 +13,7 @@ QBCore.Functions.CreateCallback("fx-garage:server:GetVehicleProps",function(sour
     cb(properties)
 end)
 
-RegisterNetEvent('fx-garage:server:SetVehicleProps',function(data,plate)
+RegisterNetEvent('qb-garages:server:SetVehicleProps',function(data,plate)
     local src = source
     local Engine = math.floor(data.engine + 0.5)
     local Body = math.floor(data.body + 0.5)
@@ -28,23 +28,23 @@ RegisterNetEvent('fx-garage:server:SetVehicleProps',function(data,plate)
     local result = exports.oxmysql:fetchSync('UPDATE player_vehicles SET engine = ?,body = ? WHERE plate= ?', {Engine,Body,plate})
 end)
 
-QBCore.Functions.CreateCallback("fx-garage:server:CheckVeh",function(source, cb, plate, citizenid)
+QBCore.Functions.CreateCallback("qb-garages:server:CheckVeh",function(source, cb, plate, citizenid)
     local src = source
     local result = exports.oxmysql:executeSync( 'SELECT citizenid FROM player_vehicles WHERE plate= ?',{ plate})
     if result[1].citizenid == citizenid then cb(true) end
 end)
 
-RegisterNetEvent('fx-garage:server:UpdateState', function(plate)
+RegisterNetEvent('qb-garages:server:UpdateState', function(plate)
     local result = exports.oxmysql:executeSync('UPDATE player_vehicles SET state = 0, garage = "" WHERE plate = ?',{plate})
 end)
 
-RegisterNetEvent('fx-garage:server:SaveCar', function(garage,plate)
+RegisterNetEvent('qb-garages:server:SaveCar', function(garage,plate)
     local Garage = tostring(garage)
     local Plate = tostring(plate)
     exports.oxmysql:execute('UPDATE player_vehicles SET state = 1, garage = ? WHERE plate = ?',{Garage, Plate})
 end)
 
-QBCore.Functions.CreateCallback('fx-garage:server:HasMoney',function(source, cb)
+QBCore.Functions.CreateCallback('qb-garages:server:HasMoney',function(source, cb)
     local Player = QBCore.Functions.GetPlayer(source)
     local Money = Player.PlayerData.money.cash
     if Money >= 2000 then
@@ -56,7 +56,7 @@ QBCore.Functions.CreateCallback('fx-garage:server:HasMoney',function(source, cb)
 
 end)
 
-QBCore.Functions.CreateCallback("fx-garage:server:GetHouseVehicles",function(source, cb, house)
+QBCore.Functions.CreateCallback("qb-garages:server:GetHouseVehicles",function(source, cb, house)
     local src = source
     local pData = QBCore.Functions.GetPlayer(src)
     exports.oxmysql:fetch('SELECT * FROM player_vehicles WHERE garage = ?', {house}, function(result)
@@ -68,7 +68,7 @@ QBCore.Functions.CreateCallback("fx-garage:server:GetHouseVehicles",function(sou
     end)
 end)
 
-QBCore.Functions.CreateCallback("qb-garage:server:checkVehicleHouseOwner",function(source, cb, plate, house)
+QBCore.Functions.CreateCallback("qb-garages:server:checkVehicleHouseOwner",function(source, cb, plate, house)
     local src = source
     local pData = QBCore.Functions.GetPlayer(src)
     exports.oxmysql:fetch('SELECT * FROM player_vehicles WHERE plate = ?',{plate}, function(result)
